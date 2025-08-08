b408dd2ff451ea0173faef3c5c97ffc79fe263ef
allan-bit-web <allankinuthia59@gmail.com>
2025-08-08 22:31:38 +0300

    added logout url

--- edulensai/authenticating/urls.py
+++ edulensai/authenticating/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import homepage,register,login,dashboard
+from .views import homepage,register,login,dashboard,logout
 
 urlpatterns = [
     path('',homepage,name="homepage"),
     path('register',register,name="register"),
-    path('login',login,name="login"),
+    path('login',login,name="log-in"),
+    path('logout',logout,name="log-out"),
     path('dashboard',dashboard,name="dashboard"), 
 ]
